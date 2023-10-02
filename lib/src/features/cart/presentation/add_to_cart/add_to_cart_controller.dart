07967c91a24d27cd8cecc5d0d3df429bc3a5e050
CarlosTSilva <carlosteixeira.tc@hotmail.com>
2023-10-02 19:26:22 -0300

    implementation shop page

---
+++ lib/src/features/cart/presentation/add_to_cart/add_to_cart_controller.dart
@@ -0,0 +1,45 @@
+import 'package:ecommerce_app/src/features/cart/application/cart_service.dart';
+import 'package:ecommerce_app/src/features/cart/domain/item.dart';
+import 'package:ecommerce_app/src/features/products/domain/product.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AddToCartController extends StateNotifier<AsyncValue<int>> {
+  AddToCartController({
+    required this.cartService,
+  }) : super(const AsyncData(1));
+
+  final CartService cartService;
+
+  void updateQuantity(int quantity) {
+    state = AsyncData(quantity);
+  }
+
+  Future<void> addItem(ProductID productID) async {
+    final item = Item(
+      quantity: state.value!,
+      productId: productID,
+    );
+
+    state = const AsyncLoading<int>().copyWithPrevious(state);
+
+    final value = await AsyncValue.guard(() => cartService.addItem(item));
+
+    if (value.hasError) {
+      state = AsyncError(value.error!, StackTrace.current);
+    } else {
+      state = const AsyncData(1);
+    }
+  }
+}
+
+// shoud use dispose?
+
+final addToCartControllerProviderProvider =
+    StateNotifierProvider.autoDispose<AddToCartController, AsyncValue<int>>(
+        (ref) {
+  return AddToCartController(
+    cartService: ref.watch(
+      cartServiseProvider,
+    ),
+  );
+});
