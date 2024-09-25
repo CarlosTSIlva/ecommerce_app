19db35e987da7896071241ff5f287e188874233d
CarlosTSIlva <carlosteixeira.tc@hotmail.com>
2024-09-25 20:51:50 -0300

    Refactor authentication and checkout controllers for Riverpod

---
+++ lib/src/features/checkout/presentation/payment/payment_button_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'payment_button_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$paymentButtonControllerHash() =>
+    r'141a0ed5c3900449a180e9f2d548890e58a725bc';
+
+/// See also [PaymentButtonController].
+@ProviderFor(PaymentButtonController)
+final paymentButtonControllerProvider =
+    AutoDisposeAsyncNotifierProvider<PaymentButtonController, void>.internal(
+  PaymentButtonController.new,
+  name: r'paymentButtonControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$paymentButtonControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$PaymentButtonController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
